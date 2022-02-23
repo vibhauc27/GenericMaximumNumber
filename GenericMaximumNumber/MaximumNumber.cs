acc102e436918aaf60c55397c2d55157b0b7170c
vibha <vibhauc@gmail.com>
2022-02-23 21:33:03 +0530

    [Vibha]Added:Generic-to find the Maximum numbers

---
+++ GenericMaximumNumber/MaximumNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMaximumNumber
+{
+    public class MaximumNumber
+    {
+
+        public static int MaximumValueofThree(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            else if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            else
+            {
+                Console.WriteLine("1st,2nd,3rd having same value/equals");
+                return 0;
+            }
+        }
+    }
+}
---
+++ GenericMaximumNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericMaximumNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find the maximum number");
+            int output = MaximumNumber.MaximumValueofThree(2, 3, 4);
+            Console.WriteLine(output);
+        }
+    }
+}
